86bd929234e7f7d5f66bc49bfb54df638d2ede28
daniil <skeal.smirnov2004@mail.ru>
2025-05-13 01:01:17 +0300

    change writer worker

--- message_inbox/workers/writer.py
+++ message_inbox/workers/writer.py
@@ -14,55 +14,56 @@ logger = get_logger(__name__)
 class MessageInboxWriterWorker:
     def __init__(self, session_maker: async_sessionmaker[AsyncSession], consumer: AIOKafkaConsumer):
         self.session_maker = session_maker
-        self.consumer = consumer
+        self.kafka_consumer = consumer
 
     async def start_consuming(self) -> None:
         logger.info("Starting consuming")
 
-        async for message in self.consumer:
-            contextvars.clear_contextvars()
+        async with self.kafka_consumer as consumer:
+            async for message in consumer:
+                contextvars.clear_contextvars()
 
-            logger.info("Received message", message=message)
+                logger.info("Received message", message=message)
 
-            try:
-                message_value = loads(message.value)
-            except JSONDecodeError:
-                logger.error("Decode error", exc_info=True)
-                continue
-
-            headers = self.parse_headers(message.headers)
-            message_id = headers.get("x-message-id")
-            trace_id = headers.get("x-trace-id")
-
-            if message_id is None:
-                logger.error("No message id")
-                continue
-
-            contextvars.bind_contextvars(
-                message_id=message_id,
-                trace_id=trace_id,
-            )
-
-            async with self.session_maker() as session:
-                repository = MessageInboxRepository(session)
-
-                db_message = await repository.get_by_id(message_id)
-                if db_message is not None:
-                    logger.info("The message has already been received", message_id=message_id)
+                try:
+                    message_value = loads(message.value)
+                except JSONDecodeError:
+                    logger.error("Decode error", exc_info=True)
                     continue
 
-                validated_message_value = KafkaMessageValueSchema(**message_value)
-                validated_message = MessageSchema(
-                    id=message_id,
-                    topic=message.topic,
-                    trace_id=trace_id,
-                    payload=validated_message_value.payload,
-                    event_type=validated_message_value.event_type,
-                )
-                await repository.create(validated_message)
-                await session.commit()
+                headers = self.parse_headers(message.headers)
+                message_id = headers.get("x-message-id")
+                trace_id = headers.get("x-trace-id")
 
-                logger.info("The message has been written to the inbox", message_id=message_id)
+                if message_id is None:
+                    logger.error("No message id")
+                    continue
+
+                contextvars.bind_contextvars(
+                    message_id=message_id,
+                    trace_id=trace_id,
+                )
+
+                async with self.session_maker() as session:
+                    repository = MessageInboxRepository(session)
+
+                    db_message = await repository.get_by_id(message_id)
+                    if db_message is not None:
+                        logger.info("The message has already been received", message_id=message_id)
+                        continue
+
+                    validated_message_value = KafkaMessageValueSchema(**message_value)
+                    validated_message = MessageSchema(
+                        id=message_id,
+                        topic=message.topic,
+                        trace_id=trace_id,
+                        payload=validated_message_value.payload,
+                        event_type=validated_message_value.event_type,
+                    )
+                    await repository.create(validated_message)
+                    await session.commit()
+
+                    logger.info("The message has been written to the inbox", message_id=message_id)
 
     @staticmethod
     def parse_headers(headers: list[tuple[str, bytes]]) -> dict[str, Any]:
